6b6d10fb16f540d497d9e6eeebe27f8851d80ad4
Atekelt <atekeltafework55@gmail.com>
2019-07-02 15:23:17 -0700

    Delete HelloWorldController.java

--- src/com/crts/HelloWorldController.java
+++
@@ -1,9 +0,0 @@
-package com.crts;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class HelloWorldController {
-    // need a controller method to show the initial HTML form
-     
-}
